4bd6936247144680af008dc916048f27e9f24265
alsaghir-zinalabdin <145858801+alsaghir-zin@users.noreply.github.com>
2025-03-22 14:31:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 def worker(thread_id):
     """A simple worker function that prints messages."""
-    for i in range(5):
+    for i in range(7):
         print(f"Thread {thread_id} is really working, iteration {i}")
         time.sleep(1)  # Simulate some work with a 1-second delay
 
